e8e3268eed74aeb4417d278d5708ad828d08e4d8
iduauto <iduautomation@gmail.com>
2024-02-14 15:19:20 +0530

    Validating MAC Address after Reboot and Reset

--- functional_sanity.py
+++ functional_sanity.py
@@ -1,4 +1,6 @@
+import locaters
 from health_check import HealthCheck
+from login import Login
 from utils import Utils
 from maintenance_functionalities import Maintenance
 
@@ -11,6 +13,67 @@ class FunctionalSanity:
         self.utils=Utils(driver)
         self.health=HealthCheck(driver)
         self.maintenance = Maintenance(driver)
+        self.login=Login(driver)
+
+    def functional_sanity_06(self):
+        logger.debug( "======================================================================================" )
+        logger.debug( 'Validating MAC Address after Reboot and Reset' )
+        try:
+            if self.health.health_check_webgui() == False:
+                logger.error( 'Device health check failed. Exiting the test.' )
+                return False
+
+            #Getting wan mac address before reboot and reset
+            self.utils.search_WebGUI("WAN Information")
+            wan_mac_address = self.utils.find_element( *locaters.WanInfo_MacAddress ).text
+            logger.debug( f'WAN MAC Address before Reboot: {wan_mac_address}' )
+
+            # Rebooting Device
+            self.maintenance.reboot()
+            self.login.WebGUI_login()
+
+            #Getting wan mac address after reboot
+            self.utils.search_WebGUI( "WAN Information" )
+            wan_mac_address_after_reboot = self.utils.find_element( *locaters.WanInfo_MacAddress ).text
+            logger.debug( f'WAN MAC Address after Reboot: {wan_mac_address_after_reboot}' )
+
+            success = 0
+            if wan_mac_address == wan_mac_address_after_reboot:
+                success += 1
+                logger.info( 'WAN MAC Address is same after Reboot ' )
+            else:
+                logger.error( 'WAN MAC Address has changed after Reboot' )
+                self.utils.get_DBGLogs()
+
+            # Reseting Device
+            self.maintenance.reset()
+            self.login.WebGUI_login()
+
+            # Getting wan mac address after reset
+            self.utils.search_WebGUI( "WAN Information")
+            wan_mac_address_after_reset = self.utils.find_element( *locaters.WanInfo_MacAddress  ).text
+            logger.debug( f'WAN MAC Address after Reset: {wan_mac_address_after_reset}' )
+
+            if wan_mac_address == wan_mac_address_after_reset:
+                success += 1
+                logger.info( 'WAN MAC Address is same after Factory Reset' )
+            else:
+                logger.error( 'WAN MAC Address has changed after Factory Reset' )
+                self.utils.get_DBGLogs()
+
+
+            if success == 2:
+                logger.info( 'MAC Address is same after Reboot and Reset' )
+                return True
+            else:
+                logger.error( 'MAC Address has changed after Reboot or Reset' )
+                return False
+
+        except Exception as e:
+            logger.error( "Error occurred while executing functional_sanity_06: %s" , str( e ) )
+            self.utils.get_DBGLogs()
+            return False
+
 
     #Multiple Reset
     def functional_sanity_01(self):
--- locaters.py
+++ locaters.py
@@ -71,6 +71,7 @@ SysInfo_ModelName = (
 
 
 #Lan Iformation
+
 LANInfo_MACAddress = (
     "/html[1]/body[1]/mainapp[1]/div[1]/div[2]/div[4]/div[1]/div[2]/div[1]/div[3]/div[1]/div[2]",
     "body mainapp div div div div div:nth-child(2) div:nth-child(1) div:nth-child(3) div:nth-child(3) div:nth-child(2)"
